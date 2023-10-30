0bbc193a28fb11965d0d36c049ebbfe8d063635d
Ethan Baker <ebaker35@terpmail.umd.edu>
2023-10-30 14:06:48 -0400

    saving before i fck everything up

--- pver/silly_ast.py
+++ pver/silly_ast.py
@@ -1,47 +1,18 @@
 # imports
 from typing import Callable
-from sys    import maxsize
-from copy   import copy, deepcopy
-from enum   import Enum
-
-DEFAULT_PRECEDENCE = {
-    "lit"   : 0,
-    "err"   : 6,
-    "paren" : 4,
-    "pow"   : 3,
-    "mul"   : 2,
-    "add"   : 1
-}
 
 # Exprs
 # generic ast definition
 class Expr:
 
-    def __init__(self, preced:int=0, value:(int|str)=None, subs:list["Expr"]=[]):
-        self.preced = preced # the precedence of the expression, higher -> evaluate earlier
+    precedence = -1
+
+    def __init__(self, value:(int|str)=None, subs:list["Expr"]=[]): # could refactor this to be l and r instead of subs
         self.value  = value  # value is where needed data is stored (eg. integer values, err messages), may need to add more types in the future, or just make your own binary data representations
         self.subs   = subs   # subs is where all sub exprs are stored
 
-    def __lt__(self, other):
-        return self.preced < other.preced
-
-    def __le__(self, other):
-        return self.preced <= other.preced
-
-    def __eq__(self, other):
-        return self.preced == other.preced
-
-    def __ne__(self, other):
-        return self.preced != other.preced
-
-    def __gt__(self, other):
-        return self.preced > other.preced
-
-    def __ge__(self, other):
-        return self.preced >= other.preced
-
     def __str__(self) -> str: 
-        rep = str(self.__class__) + ": [precedence = " + str(self.preced) + "]" # sub exprs are indented by 4 spaces
+        rep = str(self.__class__) + ": "
         if self.value != None:
             rep += " [value = " + str(self.value) + "]"
         if self.subs != []:
@@ -75,12 +46,14 @@ class Expr:
 #         | <Int> / <Int>
 class BinOp(Expr):
 
+    precedence = -1
+
     op: Callable[[Expr, Expr], Expr] = lambda l, r : EvalErr("cannot eval generic BinOp") # op is the function called when evaluating the l and r exprs, returns an expr
     err_msg: str = "binop" # err_msg is the error message included if evaluating l or r results in an error (a message of "a" will result in a final error message of "eval err, a")
 
-    def __init__(self, preced:int=0, value:(int|str)=None, subs:list["Expr"]=[]):
-        super().__init__(preced=preced, subs=subs)
-        self.preced = self.__class__.default_preced
+    def __init__(self, value:(int|str)=None, subs:list["Expr"]=[]):
+        assert len(subs) == 2
+        super().__init__(subs=subs)
     
     def eval(self) -> Expr:
         l = self.subs[0].eval() # evaluating l and r exprs
@@ -94,7 +67,7 @@ class BinOp(Expr):
 # <Int> + <Int>
 class Add(BinOp):
 
-    default_preced = DEFAULT_PRECEDENCE["add"]
+    precedence = 1
     op = lambda l, r : Int(value = l.value + r.value)
     err_msg = "add"
 
@@ -102,7 +75,7 @@ class Add(BinOp):
 # <Int> - <Int>
 class Sub(BinOp):
 
-    default_preced = DEFAULT_PRECEDENCE["add"]
+    precedence = 1
     op = lambda l, r : Int(value = l.value - r.value)
     err_msg = "sub"
 
@@ -110,7 +83,7 @@ class Sub(BinOp):
 # <Int> * <Int>
 class Mul(BinOp):
 
-    default_preced = DEFAULT_PRECEDENCE["mul"]
+    precedence = 2
     op = lambda l, r : Int(value = l.value * r.value)
     err_msg = "mul"
 
@@ -118,7 +91,7 @@ class Mul(BinOp):
 # <Int> / <Int>
 class Div(BinOp):
 
-    default_preced = DEFAULT_PRECEDENCE["mul"]
+    precedence = 2
     op = lambda l, r : Int(value = l.value // r.value)
     err_msg = "div"
 
@@ -126,7 +99,7 @@ class Div(BinOp):
 # <Int> ^ <Int>
 class Pow(BinOp):
 
-    default_preced = DEFAULT_PRECEDENCE["pow"]
+    precedence = 3
     op = lambda l, r : Int(value = l.value ** r.value)
     err_msg = "pow"
 
@@ -134,10 +107,10 @@ class Pow(BinOp):
 # generic lit definition
 class Lit(Expr):
 
-    default_preced = DEFAULT_PRECEDENCE["lit"]
+    precedence = 0
 
-    def __init__(self, preced:int=default_preced, value:(int|str)=None, subs:list["Expr"]=[]):
-        super().__init__(preced=preced, value=value)
+    def __init__(self, value:(int|str)=None, subs:list["Expr"]=[]):
+        super().__init__(value=value)
 
     def eval(self):
         return self
@@ -146,13 +119,27 @@ class Lit(Expr):
 class Int(Lit):
     pass
 
+# Parens
+# parens definition
+class Parens(Expr):
+    
+    precedence = 4
+
+    def __init__(self, value:(int|str)=None, subs:list["Expr"]=[]):
+        assert len(subs) == 1
+        super().__init__(subs=subs)
+
+    def eval(self):
+        e = self.subs[0].eval()
+        if isinstance(e, Err): # error checking and propagation
+            return EvalErr(value="parens", subs=[e])
+        return e
+
 # Errs
 # generic err definition
 class Err(Expr):
 
-    default_preced = DEFAULT_PRECEDENCE["err"]
-
-    def __init__(self, preced:int=0, value:(int|str)=None, subs:list["Expr"]=[]):
+    def __init__(self, value:(int|str)=None, subs:list["Expr"]=[]):
         super().__init__(value=value, subs=subs)
 
     def eval(self):
@@ -161,93 +148,12 @@ class Err(Expr):
 # ParseErr is used when an error occurs during token parsing
 class ParseErr(Err):
     
-    def __init__(self, preced:int=0, value:(int|str)=None, subs:list["Expr"]=[]):
+    def __init__(self, value:(int|str)=None, subs:list["Expr"]=[]):
         super().__init__(value="parse err, "+value, subs=subs)
 
 # EvalErr is used when an error occurs during ast evaluation
 class EvalErr(Err):
 
-    def __init__(self, preced:int=0, value:(int|str)=None, subs:list["Expr"]=[]):
+    def __init__(self, value:(int|str)=None, subs:list["Expr"]=[]):
         super().__init__(value="eval err, "+value, subs=subs)
 
-# functions for reforming malformed asts
-# takes a marformed ast and returns the depth first traversal of the ast in list form (all subs are also removed)
-def dft_split(ast: Expr) -> list[Expr]:
-    lst = []
-    for sub in ast.subs:
-        lst += dft_split(sub)
-    ast.subs = []
-    return lst + [ast]
-
-# takes a list that is a depth first traversal of a malformed ast and stitches the list into a well formed ast
-def dft_stitch(lst: list[Expr]) -> Expr:
-    lst_len = len(lst)
-    assert lst_len % 2 == 1 # list must be odd length
-    print("lst: " + str(lst))
-
-    # base case
-    if len(lst) == 1: 
-        return lst[0]
-
-    # recursive case
-    i = lst_len - 1
-    max_i = i
-    max_op = lst[i]
-    while i > lst_len//2:
-        if lst[i] > max_op:
-            max_op = lst[i]
-            max_i = i
-        i -= 1
-    r_i = lst_len - max_i
-    max_op.subs = [lst[r_i-1], lst[r_i]]
-    max_op.preced = -1
-    rest_lst = lst[:r_i-1] + lst[r_i+1:max_i] + lst[max_i+1:]
-    print("rest:" + str(rest_lst))
-    return dft_stitch([max_op] + rest_lst) # adds the expr to the front of the list and continues
-    
-    
-    
-    
-
-
-# one pass over expr tree to fix precedence issues (instead of making a ll1 parser)
-# lower precedence exprs must be lower (closer to leafs) on the tree
-# equal precedence exprs must have their precedence reversed
-def reform(ast: Expr):
-
-    lst = dft_split(ast)
-    print(lst)
-    #return dft_stitch(lst)
-    return ast
-
-
-    """if ast.subs != []:
-        subs = []
-        for sub in ast.subs:
-            subs.append(rectify(sub))
-        sub = min(subs)
-        if ast > sub:
-            return sub.__class__()"""
-
-    
-    
-
-
-
-    """print("rectifying!")
-    subs = []
-    for sub in self.subs: # recursive case
-        subs.append(sub.rectify())
-    
-    for sub in subs:
-        print(str(self.preced) + " " + str(sub.preced))
-        # if the child parent precedence is wrong
-        # swap the parent with the min of the children
-        if self > sub:
-            min(subs)
-            
-        # if the child (e2) and parent (p) have equal precedence
-        # build left expr tree with dfs (l then r then root)
-        elif self == sub:  
-            print("not ready!") 
-    return self"""
--- pver/silly_parser.py
+++ pver/silly_parser.py
@@ -4,19 +4,21 @@ import silly_ast    as ast
 
 # is type checking at parse time a stupid idea? would allow for a cfg?
 
-GRAMMAR = """
-<Expr>  ::= (<Expr>) 
-        |   <BinOp>
+PARSE_GRAMMAR = """
+<expr>  ::= (<Expr>)
+        |   <>
 
-<BinOp> ::= <Lit> <(+|-|*|/|^)> <Expr>
-        |   <Lit>
+<op2>
 
-<Lit>    ::= <(int)>
+<op1>   ::= <Paren> <(+|-|*|/|^)> <Expr>
+        |   <Paren>
+
+<Lit>    ::= int
 """
 
 # returns true if the first tok of ts matches the given tok class
 def match_tok(ts: list[toks.Tok], tok_class:toks.Tok):
-    if len(ts) > 0 and type(ts[0]) == tok_class:
+    if len(ts) > 0 and isinstance(ts[0], tok_class):
         return True
     return False
 
@@ -24,8 +26,9 @@ def match_tok(ts: list[toks.Tok], tok_class:toks.Tok):
 def match_toks(ts: list[toks.Tok], tok_classes: list[toks.Tok]):
     for tok_class in tok_classes:
         if match_tok(ts, tok_class):
-            return tok_class
-    return None # not neccecary, just for readability
+            return True
+    return False
+
 
 # given a list of tokens returns a well formed ast
 def parse(ts) -> ast.Expr:
@@ -35,70 +38,205 @@ def parse(ts) -> ast.Expr:
     return ast.ParseErr(value="failed to consume all tokens: " + str(ts1)) # otherwise return an error
 
 # parses an expression given a tok list, returns malformed ast
-# <Expr> ::= (<Expr>) 
-#          | <BinOp>
+# <Expr>    ::= (<Expr>) 
+#           |   <BinOp>
 def parseExpr(ts: list[toks.Tok]) -> tuple[ast.Expr, list[toks.Tok]]:
     return parseBinOp(ts) # go to state parseBinOp
 
 # parses a binary operation given a tok list, returns malformed ast
-# <BinOp> ::= <Lit> <+|-|*|/> <Expr>
-#           | <Lit>
-def parseBinOp(ts: list[toks.Tok]) -> tuple[ast.Expr, list[toks.Tok]]:
-    tok_ast_map =  {toks.Add:ast.Add,
+# <BinOp>   ::= <Lit> <+|-|*|/> <Expr> (is this correct? this isnt what the well formed ast looks like)
+#           |   <Lit>
+def parseBinOp(ts: list[toks.Tok]) -> tuple[ast.Expr, list[toks.Tok]]: # i will attempt to add precedence to this, and if i cant, i will just make a new state function for higher/ lower precedence ops
+    
+    # helper functions
+    # gets corresponding op from tok_map according to the first tok in ts
+    def get_op(ts:list[toks.Tok]):
+        tok_map =  {toks.Add:ast.Add,
                     toks.Sub:ast.Sub,
                     toks.Mul:ast.Mul,
-                    toks.Div:ast.Div,
-                    toks.Pow:ast.Pow}
-    e1, ts1 = parseParen(ts) # parsing literal
+                    toks.Div:ast.Div}
+        for key, value in tok_map.items():
+            if isinstance(ts[0], key):
+                return value
+        raise Exception("how did this happpen, are you okay?")
 
-    tc = match_toks(ts1, tok_ast_map.keys()) # gets the tok class that is matched
-    if tc != None: # if the match is successful then continue in this state 
-        e2, ts2 = parseExpr(ts1[1:]) # parsing expr (ts1[1:] consumes the +|-|*|/ token)
-        return tok_ast_map[tc](subs=[e1, e2]), ts2 # returning the appropriate binop (the one that was matched)
-
-    return e1, ts1 # go to state parseLit
-
-# parses a literal given a tok list, returns well formed ast
-# <Lit> ::= <(int)>
-def parseLit(ts: list[toks.Tok]) -> tuple[ast.Expr, list[toks.Tok]]:
-    if match_tok(ts, toks.Int):
-        return ast.Int(value=ts[0].value), ts[1:]
+    """
+    initial:
     
 
+    loop:
+    ops =  [add, sub]
+    es  =  [1, 2, 1]
 
+    op = ops.pop() -> sub
+    e2 = es.pop() -> 1
+    e1 = recur
+
+    recur:
+    ops =  [add]
+    es  =  [1, 2]
+
+    ops =  [add]
+    es  =  [1, sub(2 1)]
+
+    """
+
+    # should be able to rewrite this using full recursion
+    # shunting yard function that combines expr list and op list with correct precedence
+    def sy_combine(es, binops, precedence=0):
+
+        if binops == []:
+            print("A")
+            print(es)
+            print(binops)
+            return es[0]
+
+        assert len(binops) + 1 == len(es)
+
+        op = binops.pop()
+        print("PRECED")
+        print(op.precedence)
+        print(precedence)
+        print("")
+        print(op)
+        print("'")
+        # if an op on the right has precedence <= the op on its left
+        # then it should be evaluated after the other ops (closer to root of ast)
+        if op.precedence >= precedence: # L -> R
+            print("B")
+            print(es)
+            print(binops)
+            e2 = es.pop() # second expr is last on the stack
+            e1 = sy_combine(es, binops, precedence=op.precedence)
+            e0 = op(subs=[e1, e2])
+            return e0
+        # if the op on the right has precedence > the op to its left
+        # then it should be evaulated first (closer to the leaves of ast)
+        else: # R -> L
+            print("C")
+            print(es)
+            print(binops)
+            e2 = es.pop() # second expr is last on the stack
+            e1 = es.pop() # first expr is under the second expr
+            e0 = op(subs=[e1, e2])
+            es.append(e0)
+            return sy_combine(es, binops)
+        
+
+
+        """# ops are evaluated from R to L
+        print("")
+        print("CALLED")
+        while binops != []:
+            print("")
+            print("BINOPS")
+            print(binops)
+            print("EXPRS")
+            print(es)
+            op = binops.pop()
+            # if an op on the right has precedence <= the op on its left
+            # then it should be evaluated after the other ops (closer to root of ast)
+            if op.precedence <= precedence: # L -> R
+                e2 = es.pop() # second expr is last on the stack
+                e1 = sy_combine(es, binops, precedence=op.precedence)
+            # if the op on the right has precedence > the op to its left
+            # then it should be evaulated first (closer to the leaves of ast)
+            else: # R -> L
+                e2 = es.pop() # second expr is last on the stack
+                e1 = es.pop() # first expr is under the second expr
+
+            e0 = op(subs=[e1, e2])
+            es.append(e0)
+
+        
+        assert len(es) == 1
+        return es[0]"""
+        
+    e0, ts0 = parseParen(ts) # parsing first expr of binop
+
+    # shunting yard implementation
+    # data structs for storing bin ops and exprs
+    es:list[ast.Expr] = [e0]
+    binops:list[ast.BinOp] = []
+    min_precedence = 99
+    while match_tok(ts0, (toks.Add, toks.Sub, toks.Mul, toks.Div, toks.Pow)): # while lookahead sees a bin op
+        binops.append(get_op(ts0)) # adding the op to the list of binops
+        # parsedlit1 add parsedlit2 MUL unparsed
+        # lookahead for predictive parsing
+        if match_tok(ts0[1:], toks.OpenParen): 
+            e0, ts0 = parseParen(ts0[1:]) 
+            es.append(e0) 
+
+        elif match_tok(ts0[1:], (toks.Int)): # left recursion (precedence <= max precedence)
+            e0, ts0 = parseLit(ts0[1:]) # left recursion (<= precedence) (i dont know if this can be parseLit or has to be parseParen (should work either way due to how paren changes the structure of the ast), i just matched the grammar)
+            es.append(e0) # appending parsed expr to es 
+
+        else:
+            e0, ts0 = parseExpr(ts0[1:]) # right recursion (> precedence)
+            es.append(e0) # appending parsed expr to es 
+            break
+
+    #print("TESTING")
+    #print(binops)
+    #print(es)
+
+    #  3 * 2 + 1
+    e0 = sy_combine(es, binops)
+    print(e0)
+
+    #print(e0.tree_rep())
+    #print(ts0)
+    return e0, ts0 # go to state parseParen
+
+# <Paren>   ::= (<Expr>)
+#           |   <Lit>
 def parseParen(ts: list[toks.Tok]) -> tuple[ast.Expr, list[toks.Tok]]:
     if match_tok(ts, toks.OpenParen): # if match on open paren
         e, ts1 = parseExpr(ts[1:]) # parse expr
         if match_tok(ts1, toks.CloseParen):
-            e.preced = ast.DEFAULT_PRECEDENCE["paren"]
             return e, ts1[1:]
         return ast.ParseErr(value="unclosed parentheses"), ts1
-    
+    return parseLit(ts)
+
+# parses a literal given a tok list, returns well formed ast
+# <Lit>     ::= <(int)>
+def parseLit(ts: list[toks.Tok]) -> tuple[ast.Expr, list[toks.Tok]]:
+    if match_tok(ts, toks.Int):
+        return ast.Int(value=ts[0].value), ts[1:]
     return ast.ParseErr(value="lit"), ts[1:]
+
 
 # testing
 if __name__ == "__main__":
 
-    ts1 = lexer.lex_all("(3 * 2) - 1")
-    ts2 = lexer.lex_all("3 * 2 - 1")
+    ts1 = lexer.lex_all("4 - 3 * 2")
+    #ts2 = lexer.lex_all("3 * 2 - 1")
 
     #print("TOKENS:")
     #print(ts)
 
     ast1 = parse(ts1)
-    ast2 = parse(ts2)
+    #ast2 = parse(ts2)
 
     #print("MALFORMED AST:")
+    print("")
     print(ast1.tree_rep())
-    print(ast2.tree_rep())
+    #print(ast2.tree_rep())
 
-    #wf_ast = ast.reform(mf_ast)
+    """a, b = ast.split_ast(ast1)
+    for e in a:
+        print(e)
+    print("")
+    for e in b:
+        print(e)"""
+
+    #ast = ast.reform(ast1)
 
     #print("WELL FORMED AST:")
-    #print(wf_ast.tree_rep())
+    #print(ast.tree_rep())
 
-    #print("EVAL:")
-    #print(wf_ast.eval())
+    print("EVAL:")
+    print(ast1.eval())
 
 
 # len = 5
