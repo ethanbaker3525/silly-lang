e798315c1011785341c4a3efb88ac5d3376c5fc6
Ethan Baker <ebaker35@terpmail.umd.edu>
2023-11-13 10:23:23 -0500

    oop forgot something

--- main.c
+++ main.c
@@ -3,15 +3,12 @@
 
 int64_t _entry();
 
-void print_result(int64_t x)
-{
+void print_result(int64_t x) {
   printf("%" PRId64, x);
 }
 
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
   int64_t result;
-
   result = _entry();
   print_result(result);
   putchar('\n');
